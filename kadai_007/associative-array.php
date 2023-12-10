71a485401279739e32dcbcd5a056abd2e3a03586
darkiiig821 <147668361+darkiiig821@users.noreply.github.com>
2023-12-10 20:36:33 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題７</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+    print_r($data);
+    
+    ?>
+  </p>
+</body>
+
+</html>
